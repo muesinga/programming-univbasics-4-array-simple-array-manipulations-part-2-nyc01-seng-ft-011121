fbc3e61b7be1b43c2da1490031d86167f2182108
Aaron Muesing <muesing.music@gmail.com>
2020-12-25 23:46:14 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(a1, a2)
+ a1.concat a2
+end
+
+def using_insert(a1, a2)
+  a1.insert(4, a2)
+end
+
+def using_uniq(a1)
+  a1.uniq
+end
+
+def using_flatten(a1)
+  a1.flatten
+end
+
+def using_delete(a1, a2)
+  a1.delete(a2)
+end
+
+def using_delete_at(a1, i1)
+  a1.delete_at(i1)
+end
